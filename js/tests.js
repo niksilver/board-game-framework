d9a7dbfbaca24f920231726d3a964f7a76e4113f
Nik Silver <nik@niksilver.com>
2020-07-08 16:40:06 +0100

    Confirmed JavaScript tries to reconnect at least once.

--- js/tests.js
+++ js/tests.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var BGF = require('./board-game-framework.js');
 
-// Make a dummy websocket
+// Constructor for a dummy websocket
 var EmptyWebSocket = function() {
     this.onopen = function(evt){};
     this.onclose = function(evt){};
@@ -31,6 +31,30 @@ test('Open action creates websocket', function(t) {
 
     // Check the websocket was created
     t.equal(urlUsed, 'wss://my.test.url');
+
+    // Tell tape we're done
+    t.end();
+});
+
+test('Disconnection retries at least once', function(t) {
+    // Count the number of connections; first will succeed, then
+    // we'll cut it; second will succeed.
+    var connections = 0;
+    var websocket;
+
+    // Create a BGF with a stub websocket
+    bgf = new BGF.BoardGameFramework();
+    bgf._newWebSocket = function(url) {
+        ++connections;
+        websocket = new EmptyWebSocket();
+        return websocket;
+    };
+
+    // Do the open action, then cut the connection once
+    bgf.act({ instruction: 'Open', url: 'wss://my.test.url'});
+    t.equal(connections, 1);
+    websocket.onclose({});
+    t.equal(connections, 1);
 
     // Tell tape we're done
     t.end();
