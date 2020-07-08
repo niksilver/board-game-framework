8885d467fe075bc4e5a4500163e21bc989ce2ecc
Nik Silver <nik@niksilver.com>
2020-07-08 17:51:21 +0100

    JavaScript test that we reconnect (only) a few times.

--- js/tests.js
+++ js/tests.js
@@ -38,7 +38,7 @@ test('Open action creates websocket', function(t) {
     t.end();
 });
 
-test('Disconnection retries at least once', function(t) {
+test('Disconnection means a retry at least once', function(t) {
     // Count the number of connections; first will succeed, then
     // we'll cut it; second will succeed.
     var connections = 0;
@@ -65,6 +65,50 @@ test('Disconnection retries at least once', function(t) {
         // Tell tape we're done
         t.end();
     });
+});
+
+test('Disconnection means max 4 retries', function(t) {
+    // Count the number of connections; first will succeed, then
+    // we'll cut it; second will succeed.
+    var connections = 0;
+    var websocket;
+
+    // Create a BGF with a stub websocket
+    bgf = new BGF.BoardGameFramework();
+    bgf.toapp = function(env) {};
+    bgf._newWebSocket = function(url) {
+        ++connections;
+        websocket = new EmptyWebSocket();
+        return websocket;
+    };
+
+    // Do the open action, register onopen, then cut the connection once
+    bgf.act({ instruction: 'Open', url: 'wss://my.test.url'});
+    websocket.onopen({});
+
+    t.equal(connections, 1);
+
+    // We will repeatedly (a) check we tried to reconnect, and (b) close
+    // the connection. Eventually we should stop trying to reconnect,
+    // as indicated by the connection counter no longer incrementing.
+
+    websocket.onclose({}).then(result => {
+        t.equal(connections, 2);
+        return websocket.onclose({});
+    }).then(result => {
+        t.equal(connections, 3);
+        return websocket.onclose({});
+    }).then(result => {
+        t.equal(connections, 4);
+        return websocket.onclose({});
+    }).then(result => {
+        // Check we've not tried to connect again - same connection count
+        t.equal(connections, 4);
+    }).then(result => {
+        // Tell tape we're done
+        t.end();
+    });;
+
 });
 
 test('Connecting with bad lastnum reconnects just a few times', function(t) {
