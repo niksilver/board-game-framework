475b9589f07e96c585b2b892ec5c26a1c9f1c187
Nik Silver <nik@niksilver.com>
2020-08-02 13:09:10 +0100

    Noughts and crosses: Added lots of X image URLs.

--- examples/noughts-and-crosses/images/create-images.pl
+++ examples/noughts-and-crosses/images/create-images.pl
@@ -7,6 +7,104 @@
 capture("x", q{
 https://www.flickr.com/photos/srta_lobo/22322265/
 https://www.flickr.com/photos/miglesias/14114788851/
+https://www.flickr.com/photos/adabo/28659408095/
+https://www.flickr.com/photos/monceau/437040711/
+https://www.flickr.com/photos/duncan/338679886/
+https://www.flickr.com/photos/stomen/581113862/
+https://www.flickr.com/photos/monceau/7895949896/
+https://www.flickr.com/photos/el_ramon/49512289641/
+https://www.flickr.com/photos/thoreaudown/4462010636/
+https://www.flickr.com/photos/toofarnorth/1976928551/
+https://www.flickr.com/photos/mag3737/8049594329/
+https://www.flickr.com/photos/monceau/202058768/
+https://www.flickr.com/photos/toofarnorth/2640277187/
+https://www.flickr.com/photos/monceau/16861230960/
+https://www.flickr.com/photos/duncan/2333776824/
+https://www.flickr.com/photos/donut2d/7897244/
+https://www.flickr.com/photos/monceau/30126568570/
+https://www.flickr.com/photos/kiermacz/3461029517/
+https://www.flickr.com/photos/monceau/182858181/
+https://www.flickr.com/photos/mag3737/5645699930/
+https://www.flickr.com/photos/monceau/377018022/
+https://www.flickr.com/photos/wclaphotography/9113796592/
+https://www.flickr.com/photos/monceau/158985299/
+https://www.flickr.com/photos/mag3737/5987020486/
+https://www.flickr.com/photos/monceau/367538757/
+https://www.flickr.com/photos/mag3737/6138424021/
+https://www.flickr.com/photos/monceau/3789759262/
+https://www.flickr.com/photos/_boris/569118974/
+https://www.flickr.com/photos/jeremygetscash/3090869594/
+https://www.flickr.com/photos/mag3737/5987018910/
+https://www.flickr.com/photos/jonroman/41206611494/
+https://www.flickr.com/photos/monceau/23862771503/
+https://www.flickr.com/photos/monceau/6480338013/
+https://www.flickr.com/photos/mag3737/5987020702/
+https://www.flickr.com/photos/monceau/312983060/
+https://www.flickr.com/photos/monceau/158983348/
+https://www.flickr.com/photos/_boris/2767435675/
+https://www.flickr.com/photos/monceau/149476669/
+https://www.flickr.com/photos/monceau/158983570/
+https://www.flickr.com/photos/monceau/202049432/
+https://www.flickr.com/photos/monceau/226345416/
+https://www.flickr.com/photos/monceau/114302806/
+https://www.flickr.com/photos/falcon19880125/3029180868/
+https://www.flickr.com/photos/monceau/158983251/
+https://www.flickr.com/photos/seemypicshere_pat/4125965432/
+https://www.flickr.com/photos/monceau/3411096285/
+https://www.flickr.com/photos/jmsmytaste/107479706/
+https://www.flickr.com/photos/mag3737/5986496229/
+https://www.flickr.com/photos/monceau/184813490/
+https://www.flickr.com/photos/63465296@N07/8517164923/
+https://www.flickr.com/photos/monceau/114302718/
+https://www.flickr.com/photos/monceau/158985240/
+https://www.flickr.com/photos/monceau/226345622/
+https://www.flickr.com/photos/monceau/114303725/
+https://www.flickr.com/photos/monceau/2443794738/
+https://www.flickr.com/photos/monceau/158983196/
+https://www.flickr.com/photos/monceau/487463283/
+https://www.flickr.com/photos/monceau/487431868/
+https://www.flickr.com/photos/monceau/528795547/
+https://www.flickr.com/photos/monceau/6444139697/
+https://www.flickr.com/photos/monceau/9339599532/
+https://www.flickr.com/photos/thedepartment/29640955/
+https://www.flickr.com/photos/zunkkis/3010236037/
+https://www.flickr.com/photos/monceau/8967245205/
+https://www.flickr.com/photos/monceau/215494056/
+https://www.flickr.com/photos/monceau/2270030010/
+https://www.flickr.com/photos/monceau/566538386/
+https://www.flickr.com/photos/thomashawk/37620228412/i
+https://www.flickr.com/photos/monceau/16765172345/
+https://www.flickr.com/photos/cjsmithphotography/12432840033/
+https://www.flickr.com/photos/duncan/537708260/
+https://www.flickr.com/photos/monceau/26945844184/
+https://www.flickr.com/photos/137221047@N04/44826306025/
+https://www.flickr.com/photos/thomashawk/27809279749/
+https://www.flickr.com/photos/mag3737/6142943062/
+https://www.flickr.com/photos/monceau/244674768/
+https://www.flickr.com/photos/monceau/3582875464/
+https://www.flickr.com/photos/monceau/399308894/
+https://www.flickr.com/photos/monceau/6312749935/
+https://www.flickr.com/photos/theotherdan/484651465/
+https://www.flickr.com/photos/monceau/5709126189/
+https://www.flickr.com/photos/_boris/6002378384/
+https://www.flickr.com/photos/monceau/23345492416/
+https://www.flickr.com/photos/monceau/6242700214/
+https://www.flickr.com/photos/mag3737/19823472461/
+https://www.flickr.com/photos/mag3737/8707880575/
+https://www.flickr.com/photos/bigbabymiguel/2459376341/
+https://www.flickr.com/photos/2is3/2350657726/
+https://www.flickr.com/photos/_boris/267804756/
+https://www.flickr.com/photos/monceau/5820884646/
+https://www.flickr.com/photos/monceau/7378902494/
+https://www.flickr.com/photos/rom01/35202543195/
+https://www.flickr.com/photos/craebby/22300421726/
+https://www.flickr.com/photos/universaldilletant/25012938882/
+https://www.flickr.com/photos/29997533@N03/48786079243/
+https://www.flickr.com/photos/monceau/7645245776/
+https://www.flickr.com/photos/stencilsrx/8161016839/
+https://www.flickr.com/photos/mag3737/5792971358/
+https://www.flickr.com/photos/19779889@N00/29959871538/
+https://www.flickr.com/photos/emiliano-iko/28845305145/i
 });
 
 capture("o", q{
@@ -31,8 +129,6 @@ sub capture {
             next;
         }
 
-        # Tidy the URL
-        $url =~ s,/in/.*,/,;
         print "\n\n-------------------------- Fetching $url\n";
 
         if (fetch($url, "index.html") != 0) {
