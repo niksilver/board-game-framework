0250fc6f7fa2f97db6f9005c14383e19add70f73
Nik Silver <nik@niksilver.com>
2020-08-31 19:34:37 +0100

    Started creating some envCompare tests.

--- tests/SyncTest.elm
+++ tests/SyncTest.elm
@@ -7,6 +7,7 @@ import Test exposing (..)
 import Json.Encode as Enc
 import Json.Decode as Dec
 
+import BoardGameFramework as BGF
 import BoardGameFramework.Sync as Sync exposing (Sync)
 
 
@@ -32,3 +33,35 @@ jsonTest =
 
             Err decError ->
               Expect.fail <| "Bad decoder result: " ++ (Dec.errorToString decError)
+
+
+envCompareTest : Test
+envCompareTest =
+  let
+    earlyNum = 100
+    sameNum = 150
+    lateNum = 200
+    earlyTime = 10000
+    sameTime = 15000
+    lateTime = 20000
+    try (num1, time1) (num2, time2) expected description =
+      test description <|
+        \_ ->
+          Sync.envCompare (env num1 time1) (env num2 time2)
+          |> Expect.equal expected
+  in
+  describe "envCompare test"
+  [ "Early num + early time vs late num + late time"
+    |> try (earlyNum, earlyTime) (lateNum, lateTime) LT
+
+  ]
+
+env : Int -> Int -> BGF.Envelope ()
+env num time =
+  BGF.Peer
+    { from = "dummy.client.id"
+    , to = ["c1", "c2", "c3"]
+    , num = num
+    , time = time
+    , body = ()
+    }
