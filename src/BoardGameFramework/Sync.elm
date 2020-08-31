6b3de69ba6be140ae1a7b2ae04a5e7c521054e6e
Nik Silver <nik@niksilver.com>
2020-08-31 18:22:29 +0100

    SyncTest doesn't compile, but a rewritten Sync does - needs new tests!

--- src/BoardGameFramework/Sync.elm
+++ src/BoardGameFramework/Sync.elm
@@ -10,20 +10,29 @@ module BoardGameFramework.Sync exposing
   , mapToNext, resolve
   -- Encoding and decoding
   , encoder, decoder
+  -- Utilities
+  , envCompare
   )
 
 
 import Json.Encode as Enc
 import Json.Decode as Dec
 
+import BoardGameFramework as BGF
+
 
 -- Basics
+
+
+type Timing =
+  SomeTiming { num : Int, time : Int }
+  | NoTiming
 
 
 type Sync a =
   Sync
     { step : Int
-    , envNum : Maybe Int
+    , timing : Timing
     , value : a
     }
 
@@ -33,7 +42,7 @@ zero : a -> Sync a
 zero val =
   Sync
     { step = 0
-    , envNum = Nothing
+    , timing = NoTiming
     , value = val
     }
 
@@ -44,7 +53,7 @@ assume : a -> Sync a -> Sync a
 assume val (Sync rec) =
   Sync
     { step = rec.step + 1
-    , envNum = Nothing
+    , timing = NoTiming
     , value = val
     }
 
@@ -64,67 +73,82 @@ mapToNext : (a -> a) -> Sync a -> Sync a
 mapToNext fn (Sync rec) =
   Sync
     { step = rec.step + 1
-    , envNum = Nothing
+    , timing = NoTiming
     , value = fn rec.value
     }
 
 
--- Resolve which of two values should be considered the correct one.
-resolve : Int -> Sync a -> Sync a -> Sync a
-resolve newEnvNum (Sync recNew) (Sync recOrig) =
+{-| Resolve which of two values should be considered the correct one.
+-}
+resolve : BGF.Envelope b -> Sync a -> Sync a -> Sync a
+resolve env (Sync recNew) (Sync recOrig) =
   let
-    recNew2 = { recNew | envNum = Just newEnvNum }
+    recNew2 = { recNew | timing = timing env }
   in
-  case compare recOrig.step recNew2.step of
-    GT ->
-      Sync recOrig
-
-    LT ->
-      Sync recNew2
-
-    EQ ->
-      case recOrig.envNum of
-        Nothing ->
-          Sync recNew2
-
-        Just origEnvNum ->
-          case compare origEnvNum newEnvNum of
-            LT ->
-              Sync recOrig
-
-            GT ->
-              Sync recNew2
-
-            EQ ->
-              Sync recOrig
+  (Sync recOrig) |> Debug.log "TO be implemented!"
 
 
 -- Encoding and decoding
 
 
--- Encode a synced data value for sending to another client
+{-| Encode a synced data value for sending to another client.
+You need to supply an encoder for the data value.
+-}
 encoder : (a -> Enc.Value) -> Sync a -> Enc.Value
 encoder enc (Sync rec) =
   Enc.object
     [ ( "step", Enc.int rec.step )
-    , ( "envNum", maybeEncoder Enc.int rec.envNum )
+    , ( "timing", timingEncoder rec.timing )
     , ( "data", enc rec.value)
     ]
 
 
--- Decode a synced data value received from another client
+timingEncoder : Timing -> Enc.Value
+timingEncoder t =
+  case t of
+    SomeTiming rec ->
+      Enc.list Enc.int [ rec.num, rec.time ]
+
+    NoTiming ->
+      Enc.list Enc.int []
+
+
+{-| Decode a synced data value received from another client
+You need to supply an decoder for the data value.
+-}
 decoder : (Dec.Decoder a) -> Dec.Decoder (Sync a)
 decoder dec =
   Dec.map3
     (\mn en val ->
-      Sync { step = mn, envNum = en, value = val}
+      Sync { step = mn, timing = en, value = val}
     )
     (Dec.field "step" Dec.int)
-    (Dec.field "envNum" (maybeDecoder Dec.int))
+    (Dec.field "timing" timingDecoder)
     (Dec.field "data" dec)
 
 
--- Encodes Nothing to [], and Just x to [x]
+timingDecoder : Dec.Decoder Timing
+timingDecoder =
+  let
+    listHelp list =
+      case list of
+        [] ->
+          Dec.succeed NoTiming
+
+        _ :: [] ->
+          Dec.fail "Timing encoded as one list item only"
+
+        num :: time :: [] ->
+          Dec.succeed (SomeTiming { num = num, time = time })
+
+        _ ->
+          Dec.fail "Timing encoded as list with more than two items"
+  in
+  Dec.list Dec.int
+  |> Dec.andThen listHelp
+
+
+{-- -- Encodes Nothing to [], and Just x to [x]
 maybeEncoder : (a -> Enc.Value) -> Maybe a -> Enc.Value
 maybeEncoder enc ma =
   case ma of
@@ -148,4 +172,60 @@ maybeDecoder dec =
           Dec.succeed (Just head)
   in
   Dec.list dec
-  |> Dec.andThen innerDec
+  |> Dec.andThen innerDec--}
+
+
+-- Utilities
+
+
+timing : BGF.Envelope b -> Timing
+timing env =
+  case env of
+    BGF.Welcome rec ->
+      SomeTiming { num = rec.num, time = rec.time }
+
+    BGF.Joiner rec ->
+      SomeTiming { num = rec.num, time = rec.time }
+
+    BGF.Receipt rec ->
+      SomeTiming { num = rec.num, time = rec.time }
+
+    BGF.Peer rec ->
+      SomeTiming { num = rec.num, time = rec.time }
+
+    BGF.Leaver rec ->
+      SomeTiming { num = rec.num, time = rec.time }
+
+    BGF.Connection _ ->
+      NoTiming
+
+
+{-| Compare the order of two envelopes. A `Connection`
+envelope has no order information, so is considered as late as possible.
+So if one envelope is a `Connection` envelope then the other is considered
+"before" it... unless they're both `Connection` envelopes, in which case
+they're equal.
+-}
+envCompare : BGF.Envelope b -> BGF.Envelope b -> Order
+envCompare env1 env2 =
+  let
+    timing1 = timing env1
+    timing2 = timing env2
+  in
+  case (timing1, timing2) of
+    (NoTiming, NoTiming) ->
+      EQ
+
+    (NoTiming, SomeTiming _) ->
+      GT
+
+    (SomeTiming _, NoTiming) ->
+      LT
+
+    (SomeTiming rec1, SomeTiming rec2) ->
+      case compare rec1.time rec2.time of
+        EQ ->
+          compare rec1.num rec2.num
+
+        nonEqual ->
+          nonEqual
